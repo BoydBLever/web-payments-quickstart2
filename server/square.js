46ce6da56977ef945a0127c179deb8b3a96e990c
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-17 16:01:38 -0800

    Initial commit

--- server/square.js
+++ server/square.js
@@ -1,10 +1,10 @@
 const { ApiError, Client, Environment } = require('square');
 
-const { isProduction, SQUARE_ACCESS_TOKEN } = require('./config');
+const { isProduction, EAAAEEnbZHpLOAcWL_I9hkh3DSkPUNkHM00PCD2DmJ_mOV6a_p1mOF0LSHlS3FIs } = require('./config');
 
 const client = new Client({
   environment: isProduction ? Environment.Production : Environment.Sandbox,
-  accessToken: SQUARE_ACCESS_TOKEN,
+  accessToken: EAAAEEnbZHpLOAcWL_I9hkh3DSkPUNkHM00PCD2DmJ_mOV6a_p1mOF0LSHlS3FIs,
 });
-
+//access token is from https://developer.squareup.com/apps/sq0idp-Axuh7v9sy1WYSXubJ4lQ8A/settings
 module.exports = { ApiError, client };
