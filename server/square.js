ac5a95980ea524a8c99cf6647f6921bad8cd17d6
Boyd Lever <boydlever@stanfordhacker.com>
2023-09-18 20:06:34 -0700

    Fix linting issues in square.js

--- server/square.js
+++ server/square.js
@@ -2,14 +2,14 @@ const { ApiError, Client, Environment } = require('square');
 
 const {
   isProduction,
-  https://developer.squareup.com/apps/sq0idp-Axuh7v9sy1WYSXubJ4lQ8A/settings, 
-  //insert accesss token from this website without parens/quotes
-} = require('./config');
+  // You probably need to replace this with a valid configuration value.
+  accessToken
+ } = require('./config');
 
 const client = new Client({
   environment: isProduction ? Environment.Production : Environment.Sandbox,
-  accessToken: https://developer.squareup.com/apps/sq0idp-Axuh7v9sy1WYSXubJ4lQ8A/settings,
-  //insert access token from this website without parens/quotes
+  accessToken: null
 });
 
 module.exports = { ApiError, client };
+
