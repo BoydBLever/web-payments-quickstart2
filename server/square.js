4727b40b254a37b82bf27724368a824dee63aaf6
Boyd Lever <boydlever@stanfordhacker.com>
2023-09-18 19:47:43 -0700

    Initial commit for web-payments-quickstart2

--- server/square.js
+++ server/square.js
@@ -1,10 +1,15 @@
 const { ApiError, Client, Environment } = require('square');
 
-const { isProduction, EAAAEEnbZHpLOAcWL_I9hkh3DSkPUNkHM00PCD2DmJ_mOV6a_p1mOF0LSHlS3FIs } = require('./config');
+const {
+  isProduction,
+  https://developer.squareup.com/apps/sq0idp-Axuh7v9sy1WYSXubJ4lQ8A/settings, 
+  //insert accesss token from this website without parens/quotes
+} = require('./config');
 
 const client = new Client({
   environment: isProduction ? Environment.Production : Environment.Sandbox,
-  accessToken: EAAAEEnbZHpLOAcWL_I9hkh3DSkPUNkHM00PCD2DmJ_mOV6a_p1mOF0LSHlS3FIs,
+  accessToken: https://developer.squareup.com/apps/sq0idp-Axuh7v9sy1WYSXubJ4lQ8A/settings,
+  //insert access token from this website without parens/quotes
 });
-//access token is from https://developer.squareup.com/apps/sq0idp-Axuh7v9sy1WYSXubJ4lQ8A/settings
+
 module.exports = { ApiError, client };
